75788b839d719c4201532d4ef643e8154aefe57a
Rajiv Mothilal <rajivmothilal@gmail.com>
2020-05-27 16:06:13 +0200

    request naming standards

--- src/handlers/authorizations/{ID}/error.js
+++ src/handlers/authorizations/{ID}/error.js
@@ -46,21 +46,21 @@ module.exports = {
      * produces: application/json
      * responses: 200, 400, 401, 403, 404, 405, 406, 501, 503
      */
-  put: async (c, request, h) => {
+  put: async (c, req, h) => {
     const histTimerEnd = Metrics.getHistogram(
       'authorization_error_put',
       'Put authorization error by Id',
       ['success']
     ).startTimer()
-    const span = request.span
+    const span = req.span
     try {
-      const tags = LibUtil.getSpanTags(request, Enum.Events.Event.Type.AUTHORIZATION, Enum.Events.Event.Action.PUT)
+      const tags = LibUtil.getSpanTags(req, Enum.Events.Event.Type.AUTHORIZATION, Enum.Events.Event.Action.PUT)
       span.setTags(tags)
       await span.audit({
-        headers: request.headers,
-        payload: request.payload
+        headers: req.headers,
+        payload: req.payload
       }, EventSdk.AuditEventAction.start)
-      authorizations.forwardAuthorizationError(request.headers, request.params.ID, request.payload, span)
+      authorizations.forwardAuthorizationError(req.headers, req.params.ID, req.payload, span)
       histTimerEnd({ success: true })
       return h.response().code(Enum.Http.ReturnCodes.OK.CODE)
     } catch (err) {
