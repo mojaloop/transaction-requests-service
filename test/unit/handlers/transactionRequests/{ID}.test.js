ebfedc200fc2bc8b1fefe63ec1e3246feb6dbe14
Miguel de Barros <miguel@debarros.me>
2021-10-29 16:51:26 +0200

    added protocol version unit tests to transactionRequests

--- test/unit/handlers/transactionRequests/{ID}.test.js
+++ test/unit/handlers/transactionRequests/{ID}.test.js
@@ -66,6 +66,48 @@ describe('/transactionRequests/{ID}', () => {
       expect(response.statusCode).toBe(202)
     })
 
+    it('returns a 406 with invalid protocol version for content-type', async () => {
+      const tempProtocolVersion = JSON.parse(JSON.stringify(Config.PROTOCOL_VERSIONS)) // We want to make a deep clone of the config
+      tempProtocolVersion.CONTENT = '0.1' // This is an invalid FSPIOP protocol version
+      const headers = await Mockgen.generateRequestHeaders(path, method, resource, tempProtocolVersion)
+
+      // Arrange
+      const options = {
+        method,
+        url: path,
+        headers
+      }
+
+      // Act
+      const response = await server.inject(options)
+
+      // Assert
+      expect(response.statusCode).toBe(406)
+      expect(response.result && response.result.errorInformation && response.result.errorInformation.errorCode).toBe('3001')
+      expect(response.result && response.result.errorInformation && response.result.errorInformation.errorDescription).toBe('Unacceptable version requested - Client supplied a protocol version which is not supported by the server')
+    })
+
+    it('returns a 406 with invalid protocol version for accept-type', async () => {
+      const tempProtocolVersion = JSON.parse(JSON.stringify(Config.PROTOCOL_VERSIONS)) // We want to make a deep clone of the config
+      tempProtocolVersion.ACCEPT.DEFAULT = '0.1' // This is an invalid FSPIOP protocol version
+      const headers = await Mockgen.generateRequestHeaders(path, method, resource, tempProtocolVersion)
+
+      // Arrange
+      const options = {
+        method,
+        url: path,
+        headers
+      }
+
+      // Act
+      const response = await server.inject(options)
+
+      // Assert
+      expect(response.statusCode).toBe(406)
+      expect(response.result && response.result.errorInformation && response.result.errorInformation.errorCode).toBe('3001')
+      expect(response.result && response.result.errorInformation && response.result.errorInformation.errorDescription).toBe('Unacceptable version requested - The Client requested an unsupported version, see extension list for supported version(s).')
+    })
+
     it('handles when error is thrown', async () => {
       const headers = await Mockgen.generateRequestHeaders(path, method, resource, Config.PROTOCOL_VERSIONS)
       // Arrange
